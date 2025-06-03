443c1f9d0876cffabf331675b366a4756777ecb8
Aman <aman180604.sharma@gmail.com>
2025-06-03 16:06:15 +0530

    feature ma commit

--- aman.java
+++ aman.java
@@ -2,6 +2,6 @@ public class aman {
     public static void main(String[] args) {
         System.out.println("jai shree ram");
                 System.out.println("jai shree mahakal");
-
+System.out.println("jay mata di");
     }
 }
